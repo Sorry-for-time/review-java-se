8037c7419c3c58554df391bb10e1856f1bda4ac5
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-23 22:14:53 +0800

    æadd junit testing dependency

---
+++ chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearList.java
@@ -0,0 +1,9 @@
+package me.shalling.linearList;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling 
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling.linearList}
+* @since            2022/9/23 20:42
+* @version          v0.01
+*/public class LinearList {
+}
---
+++ chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearListTest.java
@@ -0,0 +1,9 @@
+package me.shalling.linearList;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling 
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling.linearList}
+* @since            2022/9/23 21:17
+* @version          v0.01
+*/public class LinearlistTest {
+}
