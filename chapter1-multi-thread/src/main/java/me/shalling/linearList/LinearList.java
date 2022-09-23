26c00e9415bff2666abdbc647c0ea5ebeaa1e12f
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-23 22:15:41 +0800

    add LinearList Instance Demo and test

--- chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearList.java
+++ chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearList.java
@@ -1,9 +1,129 @@
-package me.shalling.linearList;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling.linearList}
-* @since            2022/9/23 20:42
-* @version          v0.01
-*/public class LinearList {
+package me.shalling.linearList;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>一个顺序表的简单实现</h1>
+ * @description 默认插入的下标位置从 1 开始, 而不是 0
+ * @package {me.shalling.linearList}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/23 20:42
+ */
+public class LinearList<T> implements Serializable {
+    /**
+     * @description 序列化 ID 标识
+     */
+    @Serial
+    private static final long serialVersionUID = -4364246500592769953L;
+
+    /**
+     * @description 线性表的容量, 默认: 20
+     */
+    private int maxLength = 20;
+
+    /**
+     * @description 缓冲区使用到的下标位置
+     */
+    private int currentIndex;
+
+    /**
+     * @description 缓冲区数组
+     */
+    private final T[] buffer;
+
+    public LinearList() {
+        this.buffer = (T[]) (new Object[this.maxLength]);
+    }
+
+    public boolean isEmpty() {
+        return this.maxLength == 0;
+    }
+
+    public boolean isRunOut() {
+        return this.maxLength == this.currentIndex;
+    }
+
+    public LinearList(int maxLength) {
+        this.maxLength = maxLength;
+        this.buffer = (T[]) (new Object[this.maxLength]);
+    }
+
+    /**
+     * @param element        插入的新元素
+     * @param insertLocation 插入的位置
+     * @description 将新元素插入到顺序表当中
+     */
+    public void insertToList(T element, int insertLocation) {
+        // 边界判断
+        if (this.isRunOut()) {
+            throw new RuntimeException("空间已用完");
+        }
+        if (insertLocation > currentIndex + 1) {
+            throw new RuntimeException("请插入在: " + this.currentIndex + " 之前");
+        }
+        if (insertLocation < 1) {
+            throw new RuntimeException("下标非法");
+        }
+        // 采用尾插法进行插入
+        for (int i = this.currentIndex; i >= insertLocation; --i) {
+            this.buffer[i] = this.buffer[i - 1];
+        }
+        this.buffer[insertLocation - 1] = element; // 新插入的元素放置在数组对应的缓存下标位置
+        ++this.currentIndex;
+    }
+
+    /**
+     * @param newItem  新元素
+     * @param location 指定下表
+     * @description 根据索引位置更新顺序表中的指定元素
+     */
+    public void updateItemByIndex(T newItem, int location) {
+        if (location > this.currentIndex + 1 || location < 1) {
+            throw new RuntimeException("下标位置非法");
+        }
+        this.buffer[location - 1] = newItem;
+    }
+
+    public T deleteItem(int removeLocation) {
+        // 边界判断
+        if (this.isEmpty()) {
+            throw new RuntimeException("当前列表为空");
+        }
+        if (removeLocation > this.currentIndex || removeLocation < 1) {
+            throw new RuntimeException("索引位置非法");
+        }
+        // 取得对应下标的值进行保存
+        T backValue = this.buffer[removeLocation - 1];
+        // 循环进行元素向前迁移
+        for (int i = removeLocation - 1; i < this.currentIndex; ++i) {
+            this.buffer[i - 1] = this.buffer[i];
+        }
+        --this.currentIndex;
+        return backValue;
+    }
+
+
+    /**
+     * @param consumer 消费者对象
+     * @description 遍历顺序表中缓存区的实际元素
+     */
+    public void forEach(Consumer<T> consumer) {
+        for (int i = 0; i < this.currentIndex; i++) {
+            consumer.accept(this.buffer[i]);
+        }
+    }
+
+    /**
+     * @return 元素列表
+     * @description 取得缓存中的列表元素数组
+     */
+    public T[] getFactItems() {
+        T[] backValue = (T[]) new Object[this.currentIndex];
+        System.arraycopy(this.buffer, 0, backValue, 0, this.currentIndex);
+        return backValue;
+    }
 }
--- chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearListTest.java
+++ chapter1-multi-thread/src/main/java/me/shalling/linearList/LinearListTest.java
@@ -1,9 +1,55 @@
-package me.shalling.linearList;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling.linearList}
-* @since            2022/9/23 21:17
-* @version          v0.01
-*/public class LinearlistTest {
+package me.shalling.linearList;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling.linearList}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/23 21:17
+ */
+public class LinearListTest {
+    @Test
+    public void linearListTest() {
+        LinearList<String> stringLinearList = new LinearList<>(50);
+        System.out.println("当前顺序表是否为空: " + (stringLinearList.isEmpty() ? "是" : "否"));
+        // 批量插入一波没有感情的测试数据
+        for (int start = 1; start <= 10; start++) {
+            stringLinearList.insertToList(String.valueOf(start), start);
+        }
+        // 在指定位置上插入元素
+        stringLinearList.insertToList("233", 1);
+        // 分割线
+        System.out.println("=".repeat(10) + "分割线" + "=".repeat(10));
+        // 随机删除一个元素, 并查看下删除的元素
+        System.out.println("删除的元素: " + stringLinearList.deleteItem(3));
+        stringLinearList.updateItemByIndex("咦?", 7);
+        // 调用遍历: 这里传入一个消费者实例
+        System.out.println("遍历输出结果如下:");
+        stringLinearList.forEach(new Consumer<>() {
+            /**
+             * @description 用于记录当遍历的位置
+             */
+            private static int wrapFlag = 1;
+
+            @Override
+            public void accept(String s) {
+                System.out.print("|" + s + "\t");
+                // 每 输出 10 个进行换行
+                if (wrapFlag % 10 == 0) {
+                    System.out.println("|");
+                    System.out.println();
+                }
+                wrapFlag++;
+            }
+        });
+        System.out.println();
+        System.out.println("=".repeat(10) + "分割线" + "=".repeat(10));
+        System.out.println("取得 buffer 中的实际元素: " + Arrays.toString(stringLinearList.getFactItems()));
+    }
 }
