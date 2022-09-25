5eb26769966a41022bdd51516be13fefe8ad1894
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-25 16:49:55 +0800

    add a thread safe lazy singleton instance

--- chapter1-multi-thread/src/main/java/me/shalling/threadTest/LazySingleton.java
+++ chapter1-multi-thread/src/main/java/me/shalling/threadTest/LazySingleton.java
@@ -1,9 +1,27 @@
-package me.shalling.threadTest;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling.threadTest}
-* @since            2022/9/25 15:33
-* @version          v0.01
-*/public class LazySinglton {
+package me.shalling.threadTest;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling.threadTest}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/25 15:33
+ */
+public class LazySingleton {
+    private static LazySingleton lazySingleton;
+
+    private final int age = 23;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        synchronized (lazySingleton) {
+            if (null == lazySingleton) {
+                lazySingleton = new LazySingleton();
+            }
+        }
+        return lazySingleton;
+    }
 }
