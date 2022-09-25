4fc73c4b697672f72c5e0d64d1de50464c4ebac7
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-25 10:56:50 +0800

    use synchronized

--- chapter1-multi-thread/src/main/java/me/shalling/Main.java
+++ chapter1-multi-thread/src/main/java/me/shalling/Main.java
@@ -1,9 +1,35 @@
-package me.shalling;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling}
-* @since            2022/9/25 8:58
-* @version          v0.01
-*/public class Main {
+package me.shalling;
+
+import me.shalling.threadTest.RunnableExample;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/25 8:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        RunnableExample runnableExample = new RunnableExample(30);
+        Thread thread = new Thread(runnableExample);
+        Thread thread1 = new Thread(runnableExample);
+        Thread thread2 = new Thread(runnableExample);
+        Thread thread3 = new Thread(runnableExample);
+        Thread thread4 = new Thread(runnableExample);
+        Thread thread5 = new Thread(runnableExample);
+        Thread thread6 = new Thread(runnableExample);
+        Thread thread7 = new Thread(runnableExample);
+        {
+            thread.start();
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
+            thread6.start();
+            thread7.start();
+        }
+    }
 }
--- chapter1-multi-thread/src/main/java/me/shalling/threadTest/RunnableExample.java
+++ chapter1-multi-thread/src/main/java/me/shalling/threadTest/RunnableExample.java
@@ -1,22 +1,34 @@
 package me.shalling.threadTest;
 
+import lombok.NoArgsConstructor;
+
 /**
  * @author Shalling
  * @version v0.01
- * @description <h1>write comment here</h1>
+ * @description <h1>Runnable 实现 以及 synchronized 设置线程同步</h1>
  * @package {me.shalling.threadTest}
  * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
  * @since 2022/9/25 8:31
  */
-public class RunnableTest implements Runnable {
-    private int ticket = 100;
+@NoArgsConstructor
+public class RunnableExample implements Runnable {
+    private Integer ticket = 100;
     private int threadTag = 1;
+
+    public RunnableExample(int ticket) {
+        this.ticket = ticket;
+    }
 
     @Override
     public void run() {
         Thread.currentThread().setName("当前线程名称: " + threadTag++);
-        while (ticket > 0) {
-            System.out.println("当前线程" + ticket--);
+        while (true) {
+            synchronized (ticket) {
+                if (ticket < 1) {
+                    break;
+                }
+                System.out.println("当前线程: " + Thread.currentThread().getName() + " ticket: " + ticket--);
+            }
         }
     }
 }
