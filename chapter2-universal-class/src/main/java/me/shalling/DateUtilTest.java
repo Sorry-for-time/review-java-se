34dca0e3053553ffaf793d81caa2d244f87758f4
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-28 23:01:38 +0800

    字符custom reverse string

--- chapter2-universal-class/src/main/java/me/shalling/DateUtilTest.java
+++ chapter2-universal-class/src/main/java/me/shalling/DateUtilTest.java
@@ -14,6 +14,34 @@ import java.util.Date;
  * @since 2022/9/28 19:58
  */
 public class DateUtilTest {
+    /**
+     * @param source     初始字符串
+     * @param beginIndex 截取起始位置
+     * @param endIndex   截取终点位置
+     * @return 反转后的字符串
+     * @description 自定义字符串指定位置进行反转
+     * @brief 截取起始位置从 0 开始, 左闭右开
+     */
+    public static String customizeReverse(String source, int beginIndex, int endIndex) {
+        char[] chars = source.toCharArray();
+        // 初始化存储空间
+        char[] sliceStr = new char[endIndex - beginIndex];
+        // 填充数组
+        System.arraycopy(chars, beginIndex, sliceStr, 0, sliceStr.length);
+        for (int i = beginIndex; i < endIndex; i++) {
+            chars[i] = sliceStr[endIndex - i - 1];
+        }
+        return String.valueOf(chars);
+    }
+
+    @Test
+    public void stringReverseTest() {
+        String str = "HelloWorld";
+        System.out.println(str.substring(1, 3)); // el
+        String s = customizeReverse(str, 2, 5); // llo
+        System.out.println(s); // HeollWorld
+    }
+
     @Test
     public void simpleDateTest() {
         Date date = new Date();
