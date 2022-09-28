fa0d9400e87d01284bcb9f703724e4cc071a29fb
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-28 19:58:02 +0800

    add StringBuffe simple test

--- chapter2-universal-class/src/main/java/me/shalling/StringBufferTest.java
+++ chapter2-universal-class/src/main/java/me/shalling/StringBufferTest.java
@@ -1,9 +1,27 @@
-package me.shalling;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling}
-* @since            2022/9/28 19:29
-* @version          v0.01
-*/public class StringBufferTest {
+package me.shalling;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/28 19:29
+ */
+public class StringBufferTest {
+    @Test
+    public void stringBasicBufferTest() {
+        StringBuffer buffer = new StringBuffer(0x20);
+        buffer.append(12);
+        buffer.append("How");
+        int v = (int) (Math.random() * 1000);
+        System.out.println(v);
+        System.out.println();
+        for (int i = 1; i <= v; i++) {
+            buffer.append(i);
+        }
+        System.out.println(buffer);
+    }
 }
