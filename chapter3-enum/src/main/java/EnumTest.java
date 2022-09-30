202f9d14f017f51904fd3d8e0aaf28e98c6602a7
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-30 20:52:14 +0800

    use jdk5.0 enum keyword define an enum class

---
+++ chapter3-enum/src/main/java/EnumTest.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling 
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {PACKAGE_NAME}
+* @since            2022/9/30 9:28
+* @version          v0.01
+*/public class EnumTest {
+}
