94b0df0ea628c4bc9d9a465a5c18bdb855d4316e
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-05 22:19:47 +0800

    add io example test

---
+++ chapter8-io-test/src/main/java/me/shalling/IOTest.java
@@ -0,0 +1,18 @@
+package me.shalling;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since ${DATE} ${TIME}
+ */
+public class IOTest {
+    @Test
+    public void IOFirstTest() {
+
+    }
+}
