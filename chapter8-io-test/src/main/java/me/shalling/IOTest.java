b1f3c5f506427289597bdac688ba055d376d596e
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-06 22:49:39 +0800

    add File test

--- chapter8-io-test/src/main/java/me/shalling/IOTest.java
+++ chapter8-io-test/src/main/java/me/shalling/IOTest.java
@@ -2,6 +2,9 @@ package me.shalling;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.Arrays;
+
 /**
  * @author Shalling
  * @version v0.01
@@ -13,6 +16,49 @@ import org.junit.jupiter.api.Test;
 public class IOTest {
     @Test
     public void IOFirstTest() {
+        File file = new File("src/main/resources/hello.txt");
+        File file1 = new File("src/");
+        System.out.println(file.getPath());
+        System.out.println(file.getAbsolutePath());
 
+        System.out.println(Arrays.toString(file1.list()));
+        System.out.println(Arrays.toString(file1.listFiles()));
+
+        // 重命名文件
+        boolean b = file.renameTo(new File("src/main/resources/hi.txt"));
+    }
+
+    @Test
+    public void IOSecondTest() {
+        File file = new File("D:\\33306\\Documents\\test_c\\just-tmp\\1");
+        System.out.println(file.getAbsoluteFile());
+        System.out.println(file.exists());
+        System.out.println(file.isDirectory());
+        // 创建文件夹 --> 如果父目录等不存在, 那么一并创建
+        if (!file.exists()) {
+            boolean mkdirs = file.mkdirs();
+            System.out.println(mkdirs);
+            System.out.println(file.exists());
+            System.out.println(file.isDirectory());
+        }
+    }
+
+    public void listDirectory(File dist) {
+        File[] files = dist.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listDirectory(file);
+                } else {
+                    System.out.println(file.getAbsolutePath());
+                }
+            }D
+        }
+    }
+
+    @Test
+    public void IOThirdTest() {
+        File file = new File("D:\\33306\\Documents\\test_c\\just-tmp");
+        listDirectory(file);
     }
 }
