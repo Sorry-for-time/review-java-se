dbea46d8ccef16c49df346a2cd70794d638e94cb
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-09 12:47:51 +0800

    add network module test

---
+++ chapter9-network/src/main/java/me/shalling/Main.java
@@ -0,0 +1,15 @@
+package me.shalling;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
