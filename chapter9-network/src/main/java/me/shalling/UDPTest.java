3a7dbd390da072d68d5bc1d53e97f337481ee56f
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-10 19:13:48 +0800

    add udp simple programing test

--- chapter9-network/src/main/java/me/shalling/UDPTest.java
+++ chapter9-network/src/main/java/me/shalling/UDPTest.java
@@ -1,9 +1,53 @@
-package me.shalling;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling}
-* @since            2022/10/10 18:49
-* @version          v0.01
-*/public class UDPTest {
+package me.shalling;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>UDP 网络编程简单测试</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/10/10 18:49
+ */
+public class UDPTest {
+    /**
+     * @description 发送端测试
+     */
+    @Test
+    public void sendTest() {
+        try (
+                DatagramSocket datagramSocket = new DatagramSocket();
+        ) {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String message = "how are you today?";
+            byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+            datagramSocket.send(new DatagramPacket(messageBytes, messageBytes.length, inetAddress, 6553));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @description 接收端测试
+     */
+    @Test
+    public void receiveTest() {
+        try (
+                DatagramSocket datagramSocket = new DatagramSocket(6553);
+        ) {
+            byte[] bytes = new byte[100];
+            DatagramPacket datagramPacket = new DatagramPacket(bytes, bytes.length);
+            datagramSocket.receive(datagramPacket);
+            System.out.println("receive value" + ": " + new String(datagramPacket.getData(), 0, datagramPacket.getLength()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
