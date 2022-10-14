fbbcbf4d8e97a244034ce22ba73574779aeef017
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-14 22:10:47 +0800

    add a dynamic proxy instance test

---
+++ chapter10-reflect/src/main/java/me/shalling/DynamicProxyTest.java
@@ -0,0 +1,77 @@
+package me.shalling;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>动态代理测试</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/10/14 21:25
+ */
+public class DynamicProxyTest {
+    @Test
+    public void dynamicProxyTest() {
+        Creature proxyInstance = (Creature) ProxyFactory.getProxyInstance(new Human());
+        proxyInstance.showEager();
+    }
+}
+
+/**
+ * @description 测试类的父接口
+ */
+interface Creature {
+    void showEager();
+}
+
+/**
+ * @description 测试类
+ */
+class Human implements Creature {
+    @Override
+    public void showEager() {
+        System.out.println("JUST WANT LIFE RUN AND GO");
+    }
+}
+
+/**
+ * @description 自定义 InvocationHandler 实现类
+ */
+final class MyInvocationHandler implements InvocationHandler {
+    private final Object originTarget;
+
+    public MyInvocationHandler(Object originTarget) {
+        this.originTarget = originTarget;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 注: invoke 方法需要取得原始的被代理类对象
+        return method.invoke(this.originTarget, args);
+    }
+}
+
+final class ProxyFactory {
+    public static Object getProxyInstance(Object origin) {
+        final InvocationHandler invocationHandler = new MyInvocationHandler(origin);
+        /*
+         * 在通过代理类对象执行被代理类对象的方法时, 会通过传入的实现 InvocationHandler 接口对象的 invoke 方法
+         * 执行被代理类的对应方法, 并返回对应的值 -> 内部依然通过 invoke(被代理类对象, 参数)的形式来实现方法调用
+         * -> 为切面编程提供了条件
+         **/
+        return Proxy
+                .newProxyInstance(
+                        origin
+                                .getClass()
+                                .getClassLoader(),
+                        origin
+                                .getClass()
+                                .getInterfaces(),
+                        invocationHandler);
+    }
+}
