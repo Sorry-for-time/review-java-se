bc4dd5ef62617939f6b088f5cab539d52d434fdc
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-14 22:31:29 +0800

    add java8 new features test

---
+++ chapter11-jdk8-new-features/src/main/java/me/shalling/Main.java
@@ -0,0 +1,12 @@
+package me.shalling;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling 
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling}
+* @since            ${DATE} ${TIME}
+* @version          v0.01
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
