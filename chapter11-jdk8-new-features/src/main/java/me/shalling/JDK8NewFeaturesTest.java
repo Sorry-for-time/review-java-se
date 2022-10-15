60428e14c00a57349eced2c60d988e05e84e5f1b
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-15 10:28:38 +0800

    add a lambda use test

--- chapter11-jdk8-new-features/src/main/java/me/shalling/JDK8NewFeaturesTest.java
+++ chapter11-jdk8-new-features/src/main/java/me/shalling/JDK8NewFeaturesTest.java
@@ -1,9 +1,25 @@
-package me.shalling;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling}
-* @since            2022/10/14 22:33
-* @version          v0.01
-*/public class JDK8NewFeaturesTest {
+package me.shalling;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>JAVA8 新特性使用测试</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/10/14 22:33
+ */
+public class JDK8NewFeaturesTest {
+    @Test
+    public void lambdaTest() {
+        Comparator<Integer> comparator = (o1, o2) -> {
+            System.out.println(o1 + ": " + o2);
+            return Integer.compare(o1, o2);
+        };
+        System.out.println(comparator.compare(12, 1));
+    }
+
 }
