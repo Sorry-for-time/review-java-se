cec54505f056cc91b7f06d1ae55dd467fcaaf554
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-17 23:05:29 +0800

    add dependencies

---
+++ chapter12-jdk9-after-features/src/main/java/me/shalling/Creature.java
@@ -0,0 +1,9 @@
+package me.shalling;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling 
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling}
+* @since            2022/10/17 22:54
+* @version          v0.01
+*/public interface Creature {
+}
