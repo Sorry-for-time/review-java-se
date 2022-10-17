276b86c85841c929f1696acbe9793807d2ffd9f4
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-17 22:53:32 +0800

    add jdk9 and newer version features test

---
+++ chapter12-jdk9-after-features/src/main/java/me/shalling/JDK9AfterFeaturesTest.java
@@ -0,0 +1,13 @@
+package me.shalling;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>write comment here</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since ${DATE} ${TIME}
+ */
+public class JDK9AfterFeaturesTest {
+
+}
