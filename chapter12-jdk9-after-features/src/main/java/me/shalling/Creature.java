ff80a42a524d624a6e653597549e6522cb15cb24
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-17 23:32:27 +0800

    jdk9 interface private method test

--- chapter12-jdk9-after-features/src/main/java/me/shalling/Creature.java
+++ chapter12-jdk9-after-features/src/main/java/me/shalling/Creature.java
@@ -1,9 +1,34 @@
-package me.shalling;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling 
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling}
-* @since            2022/10/17 22:54
-* @version          v0.01
-*/public interface Creature {
+package me.shalling;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>JDK9 接口新属性测试</h1>
+ * @package {me.shalling}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/10/17 22:54
+ */
+public interface Creature {
+    /**
+     * @description jdk9 之后允许接口里定义私有方法
+     */
+    private void consume() {
+        System.out.println("here just a simple test");
+    }
+
+    /**
+     * @description 此时方法不是静态的, 无法直接通过接口调用, 需要接口的实现类对象去调用
+     */
+    default void invokeConsume() {
+        this.consume();
+    }
+
+    /**
+     * @description 只允允许通过接口去调用
+     */
+    static void foo() {
+        System.out.println("foo");
+    }
+
+    void printFlag(String str);
 }
--- chapter12-jdk9-after-features/src/main/java/me/shalling/JDK9AfterFeaturesTest.java
+++ chapter12-jdk9-after-features/src/main/java/me/shalling/JDK9AfterFeaturesTest.java
@@ -1,13 +1,20 @@
 package me.shalling;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Shalling
  * @version v0.01
- * @description <h1>write comment here</h1>
+ * @description <h1>默认测试</h1>
  * @package {me.shalling}
  * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
- * @since ${DATE} ${TIME}
  */
 public class JDK9AfterFeaturesTest {
-
+    @Test
+    public void interfaceAllSpecifiedMethodTest() {
+        Creature creature = System.out::println;
+        creature.invokeConsume();
+        creature.printFlag("how are you today");
+        Creature.foo();
+    }
 }
