3daeb72dc99c6a0618611ce62c9985a37c374aae
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-04 14:36:21 +0800

    add jdbc test module

---
+++ chapter9-network/src/main/java/me/shalling/serverAndClient/ClientSocketSimulationTest.java
@@ -0,0 +1,14 @@
+package me.shalling.serverAndClient;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @package {me.shalling.serverAndClient}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/11/3 23:17
+ */
+public class ClientSocketSimulationTest {
+  public static void main(String[] args) {
+
+  }
+}
