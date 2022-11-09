e6c2e0f0407de0593ae105b34e84aa0ac03b8068
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-09 22:27:11 +0800

    add a server respond status enum

---
+++ chapter3-enum/src/main/java/QUERY_STATUS.java
@@ -0,0 +1,57 @@
+/**
+ * 服务器响应状态码设置
+ *
+ * @author Shalling
+ * @version v0.01
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/11/9 22:09
+ */
+public enum QUERY_STATUS {
+  /**
+   * 请求成功
+   */
+  SUCCESS(201, "请求成功"),
+
+  /**
+   * 单位时间内重复请求过多
+   */
+  VISIT_LIMIT(300, "请求过于频繁"),
+
+  /**
+   * 参数错误
+   */
+  PARAM_ERROR(401, "参数错误"),
+
+  /**
+   * 访问资源未找到
+   */
+  RESOURCE_NOT_FOUND(404, "资源未找到"),
+
+  /**
+   * 服务器器处理错误
+   */
+  SERVER_ERROR(501, "请求出错了");
+
+  /**
+   * 状态码
+   */
+  private final int CODE;
+
+  /**
+   * 描述
+   */
+  private final String DESCRIPTION;
+
+  QUERY_STATUS(int CODE, String DESCRIPTION) {
+    this.CODE = CODE;
+    this.DESCRIPTION = DESCRIPTION;
+  }
+
+  public int GET_CODE() {
+    return this.CODE;
+  }
+
+  public String GET_DESCRIPTION() {
+    return this.DESCRIPTION;
+  }
+}
---
+++ chapter3-enum/src/main/java/TestStatusEnum.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;/**
+* @author           Shalling
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {PACKAGE_NAME}
+* @since            2022/11/9 22:18
+* @version          v0.01
+*/public class TestStatusEnum {
+}
